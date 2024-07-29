8d6d83d92490ad0b0f17451893f5f51c8a042eca
PhxHG <hugo@assiah.me>
2024-07-29 23:41:02 +0200

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,12 @@
+print('Hello World!')
+# this is the first day of the 100 days of Python challenge
+'''
+this is a multiline comment
+
+Python compiles and runs code real-time via CPython
+
+Python ia a multifunction(functional and object oriented) interpreted language
+Java is a compiled object oriented language
+Compiler converts code intro machine code or c code permanently - interpreter compiles it dynamically via 'interpreting' it to lower languafges'
+'''
+
